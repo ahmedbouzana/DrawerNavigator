43fc0ad15e57ce8543bdd7e5f8c9a3e6650de049
ahmedbouzana <ah.bouzana@gmail.com>
2019-04-23 23:48:46 +0200

    Update

--- App.js
+++ App.js
@@ -6,26 +6,26 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {DrawerNavigator} from 'react-navigation';
-import {Home} from './app/components/Home'
-import {Dashboard} from './app/components/Dashboard'
-import Sidebar from './app/components/Sidebar'
-
+import React, { Component } from "react";
+import { DrawerNavigator } from "react-navigation";
+import Home from "./app/components/Home";
+import Dashboard from "./app/components/Dashboard";
+import Sidebar from "./app/components/Sidebar";
 
 class App extends Component {
   render() {
-    return (
-      <AppStack/>
-    );
+    return <AppStack />;
   }
 }
 
-const AppStack = DrawerNavigator({
-  home: {screen:Home},
-  dashboard:{screen: Dashboard}
-},{
-  contentComponent: Sidebar
-})
+const AppStack = DrawerNavigator(
+  {
+    home: { screen: Home },
+    dashboard: { screen: Dashboard }
+  },
+  {
+    contentComponent: Sidebar
+  }
+);
 
-export default App
+export default App;
